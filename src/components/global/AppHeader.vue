e3d0f24ce2d1d3308841e7467d49c216ac2684f3
andro <androadel699@gmail.com>
2021-03-17 11:31:00 +0200

    commit

--- src/components/global/AppHeader.vue
+++ src/components/global/AppHeader.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="header">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link> |
-            <router-link to="/help">Help</router-link>
-        </div>
+        <div class="logo">Elzero Vue</div>
+        <ul>
+            <router-link to="/" tag="li" exact>
+                <a>Home</a>
+            </router-link>
+            |
+            <router-link to="/about" tag="li">
+                <a>About</a>
+            </router-link>
+            |
+            <router-link to="/help" tag="li" replace>
+                <a>Help</a>
+            </router-link>
+        </ul>
     </div>
 </template>
 
@@ -13,3 +22,31 @@ export default {
     name: "AppHeader"
 };
 </script>
+
+<style lang="scss" scoped>
+.header {
+    background-color: #333;
+    color: #fff;
+    overflow: hidden;
+    .logo {
+        font-size: 24px;
+        padding: 20px;
+        float: left;
+        width: 200px;
+    }
+    ul {
+        float: left;
+        width: calc(100% - 200px);
+        li {
+            padding: 20px;
+            display: inline-block;
+            font-size: 18px;
+
+            a {
+                color: #fff;
+                text-decoration: none;
+            }
+        }
+    }
+}
+</style>
